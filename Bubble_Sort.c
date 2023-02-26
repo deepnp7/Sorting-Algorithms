b895af75c6c259b323cf096c7f5a75b24adcafd8
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-02-26 11:06:44 +0530

    Update and rename Bubble Sort.c to Bubble_Sort.c

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -5,14 +5,14 @@ void bubbleSort(int* A,int n)
    for (int i = 0; i <=n; i++)
    {
     for (int j=0;j<=n-i-1;j++)
-    {
+     {
         if(A[j]>A[j+1])
         {
             int temp= A[j];
             A[j]=A[j+1];
             A[j+1]=temp;
         }
-    }
+     }
    } 
 }
 
