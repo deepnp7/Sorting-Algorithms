e695dfc137fbabdd5db50f64859a7c792a02e03a
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-02-27 01:31:32 +0530

    Update Merge_Sort.c

--- Merge_Sort.c
+++ Merge_Sort.c
@@ -62,7 +62,7 @@ void mergeSort(int A[], int low, int high){
 
 int main()
 {
-    // int A[] = {9, 14, 4, 8, 7, 5, 6};
+   
     int A[] = {9, 1, 4, 14, 4, 15, 6};
     int n = 7;
     printArray(A, n);
