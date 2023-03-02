9e4c7ab3347c050b0cc8033921c134301d674c0a
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-02 18:45:33 +0530

    Update Quick_Sort.c

--- Quick_Sort.c
+++ Quick_Sort.c
@@ -1,6 +1,51 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
 
+void quickSort(int arr[], int left, int right) {
+    int i = left, j = right;
+    int temp;
+    int pivot = arr[(left + right) / 2];
+
+    // partition
+    while (i <= j) {
+        while (arr[i] < pivot) {
+            i++;
+        }
+        while (arr[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    // recursion
+    if (left < j) {
+        quickSort(arr, left, j);
+    }
+    if (i < right) {
+        quickSort(arr, i, right);
+    }
 }
 
+int main() {
+    int arr[] = { 7, 3, 9, 4, 6, 1, 8, 5, 2 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
