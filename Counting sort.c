a64e943163dbbac7b6a23da09864d7f974114c08
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-11 20:11:42 +0530

    Update Counting sort.c

--- Counting sort.c
+++ Counting sort.c
@@ -51,7 +51,7 @@ int main() {
   int array[] = {5, 2, 8, 3, 6, 1, 7, 9, 4};
   int size = sizeof(array) / sizeof(array[0]);
 
-  printf("Original array:\n");
+  printf("Unsorted array:\n");
   print_array(array, size);
 
   counting_sort(array, size);
