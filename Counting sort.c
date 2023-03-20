5bb751758c4d6f27f5a8323fe337406cc326966e
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-20 11:31:26 +0530

    Update Counting sort.c

--- Counting sort.c
+++ Counting sort.c
@@ -7,7 +7,8 @@ void counting_sort(int array[], int size)
   int max = array[0];
   for (int i = 1; i < size; i++)
    {
-    if (array[i] > max) {
+    if (array[i] > max)
+    {
       max = array[i];
     }
   }
