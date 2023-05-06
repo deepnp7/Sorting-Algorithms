55336b322ea822bfa15c225272d3a0bd651f867a
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-05-06 10:49:30 +0530

    Update Bogo sort(Monkey sort).c

--- Bogo sort(Monkey sort).c
+++ Bogo sort(Monkey sort).c
@@ -1,1 +1,57 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Function to check if the array is sorted
+int isSorted(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i+1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Function to shuffle the array randomly
+void shuffle(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int j = rand() % n;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+// Function to perform Bogo sort
+void bogoSort(int arr[], int n) {
+    while (!isSorted(arr, n)) {
+        shuffle(arr, n);
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Driver code
+int main() {
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    srand(time(NULL)); // Seed for the random number generator
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bogoSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
