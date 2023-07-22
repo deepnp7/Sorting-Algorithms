4eaee8489080065c9f6196f996f95e5886fced20
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-07-23 00:26:10 +0530

    Update Bogo sort(Monkey sort).c

--- Bogo sort(Monkey sort).c
+++ Bogo sort(Monkey sort).c
@@ -5,15 +5,7 @@
 
 
 
-// Function to shuffle the array randomly
-void shuffle(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        int j = rand() % n;
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-}
+
 
 // Function to perform Bogo sort
 void bogoSort(int arr[], int n) {
