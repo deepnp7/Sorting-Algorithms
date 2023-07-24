bb0a4d83c414d0703ad9bcc0ca0f1ce26075c1fd
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-07-24 20:35:14 +0530

    Update Counting_Sort.c

--- Counting_Sort.c
+++ Counting_Sort.c
@@ -2,14 +2,7 @@
 
 
 
- 
 
-int main() {
-  int array[] = {5, 2, 8, 3, 6, 1, 7, 9, 4};
-  int size = sizeof(array) / sizeof(array[0]);
-
-  printf("Unsorted array:\n");
-  print_array(array, size);
 
   counting_sort(array, size);
 
