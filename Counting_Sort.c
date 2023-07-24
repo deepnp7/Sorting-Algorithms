524b69b986c9661d66c9cf17ded7706da9096c6e
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-07-24 20:35:07 +0530

    Update Counting_Sort.c

--- Counting_Sort.c
+++ Counting_Sort.c
@@ -3,14 +3,6 @@
 
 
  
-  
-on to print an array
-void print_array(int array[], int size) {
-  for (int i = 0; i < size; i++) {
-    printf("%d ", array[i]);
-  }
-  printf("\n");
-}
 
 int main() {
   int array[] = {5, 2, 8, 3, 6, 1, 7, 9, 4};
