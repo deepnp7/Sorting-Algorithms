584689107ef3579f118f8824f50625c052989f9b
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-07-25 19:36:50 +0530

    Update Cycle_Sort.c

--- Cycle_Sort.c
+++ Cycle_Sort.c
@@ -1,13 +1,1 @@
 
-
-int main() {
-    int arr[] = {5, 2, 8, 9, 4, 3, 1, 0, 7, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cycleSort(arr, n);
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return 0;
-}
