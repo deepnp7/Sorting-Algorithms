e7faaf5e77d0bb983bc831550375cd1f88a300f8
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-08-01 18:21:13 +0530

    Update Gnome_Sort.c

--- Gnome_Sort.c
+++ Gnome_Sort.c
@@ -1,17 +1,5 @@
 
 
-}
-
-int main() {
-    int arr[] = {10, 5, 8, 3, 2};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
     gnome_sort(arr, n);
 
     printf("Sorted array: ");
