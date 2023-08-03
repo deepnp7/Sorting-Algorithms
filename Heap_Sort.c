6aa1362dce4199a402fb265e4ab0b08d291fd292
Deepnarayan Patra <deepnp7@gmail.com>
2023-08-03 22:54:30 +0530

    Update Heap_Sort.c

--- Heap_Sort.c
+++ Heap_Sort.c
@@ -2,21 +2,7 @@
     int largest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
-    if (left < heap_size && heap[left] > heap[largest
-    {
-}
-
-void heap_to_array(int *heap, int heap_size, int *array) 
-{
-    for (int i = 0; i < heap_size; i++) 
-    {
-        array[i] = heap[i];
-    }
-}
-
-int main() 
-{
-    int heap[] = {54,12,35,10,28,25,38,89,26};
+    if
     int heap_size = sizeof(heap) / sizeof(heap[0]);
     build_max_heap(heap, heap_size);
     int array[heap_size];
