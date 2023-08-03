e35d797cdd4583889e60c72065771646c0545896
Deepnarayan Patra <deepnp7@gmail.com>
2023-08-03 22:54:37 +0530

    Update Heap_Sort.c

--- Heap_Sort.c
+++ Heap_Sort.c
@@ -3,14 +3,3 @@
     int left = 2*i + 1;
     int right = 2*i + 2;
     if
-    int heap_size = sizeof(heap) / sizeof(heap[0]);
-    build_max_heap(heap, heap_size);
-    int array[heap_size];
-    printf("The sorted Array is : ");
-    heap_to_array(heap, heap_size, array);
-    for (int i = 0; i < heap_size; i++) 
-    {
-        printf("%d ", array[i]);
-    }
-    return 0;
-}
