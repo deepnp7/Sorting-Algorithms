b41135f03574bc9d021bf1783e1a7d666d107201
Deepnarayan Patra <deepnp7@gmail.com>
2023-08-09 00:49:30 +0530

    Update Insertion_Sort.cpp

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -1,14 +1,1 @@
-#include<stdio.h>
 
-
- 
- int main()
- {
-    int A[]={7,11,9,2,17,4};
-    int n=6;
-    printf("UNSORTED ARRAY : ");
-    printArray(A,n);
-    ins(A,n);
-    printf("  SORTED ARRAY : ");
-    printArray(A,n);
- }
