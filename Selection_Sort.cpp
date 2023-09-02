f1fb5f297f288ab4253ddee7bba79f39778adfae
Deepnarayan Patra <deepnp7@gmail.com>
2023-09-02 12:41:26 +0530

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,20 +1,2 @@
 
-            smallest=j;
-           }
-           temp = A[i];
-           A[i] = A[smallest];
-           A[smallest]= temp;
-        }
-    }
-}
-int main()
-{
-    int A[]={7,11,9,2,17,4};
-    int n=6;
-    printf("UNSORTED ARRAY : ");
-     printArray(A,n);
-     selectionSort(A,n);
-    printf(" SORTED ARRAY : ");
-     printArray(A,n);
-    return 0;
-}
+ 
