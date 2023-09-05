1c64ee658f16c07dafde220d4a6e9c6f0759e09c
Deepnarayan Patra <deepnp7@gmail.com>
2023-09-05 16:57:27 +0530

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,2 +1,34 @@
 
- 
+ #include <iostream>
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        // Find the minimum element in the unsorted part of the array
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    selectionSort(arr, size);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
