c04ec94605f7c6bc5355d652db3981b20da0039d
Deepnarayan Patra <deepnp7@gmail.com>
2023-09-29 18:05:34 +0530

    Update Quick_Sort.cpp

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,14 +1,6 @@
 
   
     
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Unsorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    quickSort(arr, 0, n - 1);
 
     printf("\nSorted array: ");
     for (int i = 0; i < n; i++) {
