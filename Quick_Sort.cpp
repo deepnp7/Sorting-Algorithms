efcb1f74d1ac5ee5bf53324f22722fdf7ce8a340
Deepnarayan Patra <deepnp7@gmail.com>
2023-09-29 18:05:27 +0530

    Update Quick_Sort.cpp

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,16 +1,6 @@
 
   
-    // recursion
-    if (left < j) {
-        quickSort(arr, left, j);
-    }
-    if (i < right) {
-        quickSort(arr, i, right);
-    }
-}
-
-int main() {
-    int arr[] = { 7, 3, 9, 4, 6, 1, 8, 5, 2 };
+    
     int n = sizeof(arr) / sizeof(arr[0]);
 
     printf("Unsorted array: ");
