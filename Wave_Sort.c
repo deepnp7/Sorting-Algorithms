de3cc41508a01cb091e73f4c7d0f7572c01dc000
Deepnarayan Patra <deepnp7@gmail.com>
2023-09-30 10:33:16 +0530

    Update Wave_Sort.c

--- Wave_Sort.c
+++ Wave_Sort.c
@@ -4,13 +4,7 @@
 int main() {
     int arr[] = {4, 1, 7, 3, 6, 9, 2, 8, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    
-    waveSort(arr, n);
+
     
     printf("\nWave sorted array: ");
     for (int i = 0; i < n; i++) {
