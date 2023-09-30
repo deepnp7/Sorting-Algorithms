ae5dea2b18fff123ad3dc6fb4e1a68775c2b2653
Deepnarayan Patra <deepnp7@gmail.com>
2023-09-30 10:32:46 +0530

    Update Wave_Sort.c

--- Wave_Sort.c
+++ Wave_Sort.c
@@ -1,12 +1,7 @@
 
 #include <stdio.h>
 
-void waveSort(int arr[], int n) {
-    for (int i = 0; i < n; i += 2) {
-        if (i > 0 && arr[i] < arr[i-1])
-            swap(&arr[i], &arr[i-1]);
-        if (i < n-1 && arr[i] < arr[i+1])
-            swap(&arr[i], &arr[i+1]);
+
     }
 }
 
