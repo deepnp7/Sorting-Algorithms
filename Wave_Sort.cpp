a9c8cf648d3d1f853cca9a2177704629317edb13
Deepnarayan Patra <deepnp7@gmail.com>
2023-10-01 17:58:17 +0530

    Update Wave_Sort.cpp

--- Wave_Sort.cpp
+++ Wave_Sort.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <algorithm>
 
-
+void waveSort(int arr[], int size) {
+    // Traverse all even-indexed elements
+    for (int i = 0; i < size; i += 2) {
+        // If the current even element is smaller than the previous element, swap them
+        if (i > 0 && arr[i] < arr[i - 1]) {
+            std::swap(arr[i], arr[i - 1]);
+        }
 
 
