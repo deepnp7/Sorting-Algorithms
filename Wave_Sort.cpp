788873a30d1348b1621f889b152e8435e7737e76
Deepnarayan Patra <deepnp7@gmail.com>
2023-10-01 17:59:02 +0530

    Update Wave_Sort.cpp

--- Wave_Sort.cpp
+++ Wave_Sort.cpp
@@ -19,5 +19,12 @@ void waveSort(int arr[], int size) {
 int main() {
     int arr[] = {10, 5, 6, 3, 2, 20, 100, 80};
     int size = sizeof(arr) / sizeof(arr[0]);
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    waveSort(arr, size);
 
 
