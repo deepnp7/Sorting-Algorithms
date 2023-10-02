4721cf7d2c3761191862f820cec01c791376d3f5
Deepnarayan Patra <deepnp7@gmail.com>
2023-10-02 16:21:34 +0530

    Update cocktail_Sort.c

--- cocktail_Sort.c
+++ cocktail_Sort.c
@@ -1,12 +1,4 @@
 
-    }
-}
-
-int main() {
-    int arr[] = {5, 2, 6, 1, 3, 9, 4, 8, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cocktail_sort(arr, n);
-
     printf("Sorted array: ");
     for (int i = 0; i < n; ++i) {
        printf("%d ", arr[i]);
