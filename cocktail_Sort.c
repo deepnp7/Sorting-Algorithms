b0e886deee41f00e038cb4142006b26482bd59c3
Deepnarayan Patra <deepnp7@gmail.com>
2023-10-02 16:20:54 +0530

    Update cocktail_Sort.c

--- cocktail_Sort.c
+++ cocktail_Sort.c
@@ -1,13 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-void cocktail_sort(int arr[], int n) 
-{
-    bool swapped = true;
-    int start = 0, end = n - 1;
-      while (swapped) 
-      {
-        swapped = false;
         for (int i = start; i < end; ++i) 
         {
             if (arr[i] > arr[i + 1]) 
