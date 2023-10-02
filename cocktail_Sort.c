8dd45ed473db8932ab6022531265bf0a1021b8b7
Deepnarayan Patra <deepnp7@gmail.com>
2023-10-02 16:21:26 +0530

    Update cocktail_Sort.c

--- cocktail_Sort.c
+++ cocktail_Sort.c
@@ -1,16 +1,4 @@
 
-        if (!swapped) break;
-        swapped = false;
-        --end;
-         for (int i = end - 1; i >= start; --i) {
-            if (arr[i] > arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
-            }
-        }
-       ++start;
     }
 }
 
